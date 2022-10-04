3b48f546f78e0b1fd52a5d02910c8429d16a1440
FahmiPitak <115027526+FahmiPitak@users.noreply.github.com>
2022-10-04 23:17:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
+var menu = document.getElementsByClassName('menu')[0];
+
+tombolMenu.onclick = function() {
+    menu.classList.toggle('active');
+
+<script src="script.js" defer></script>
+}
